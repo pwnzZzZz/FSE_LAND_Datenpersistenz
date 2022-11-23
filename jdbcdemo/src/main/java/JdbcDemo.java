c30f7ebcdcefd8513433afedd6fea0bb3b903936
pwnz <marceschranz@tsn.at>
2022-11-23 11:28:27 +0100

    JDBC - Daten abfragen 2.0

--- jdbcdemo/src/main/java/JdbcDemo.java
+++ jdbcdemo/src/main/java/JdbcDemo.java
@@ -11,6 +11,34 @@ public class JdbcDemo {
         updateStudentDemo(4, "Neuer Name", "neueemail@provider.at");
         selectAllDemo();
         deleteStudentDemo(9);
+        findAllByName();
+    }
+
+    public static void findAllByName(){
+        System.out.println("Find all by Name DEMO mit JDBC");
+        String sqlSelechtAllPersons = "SELECT * FROM `student`";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)){
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `student`");
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                int id = rs.getInt(1);
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+
+                System.out.println("Student aus der DB: [ID] " + id + " [Name] " + name + " [Email] " + email);
+
+
+            }
+
+        } catch(SQLException e){
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
     }
 
     public static void deleteStudentDemo(int studentID){
