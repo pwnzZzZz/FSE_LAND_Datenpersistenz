54e5c705f3a3259654a45bb1d8e4638ebfe2730f
pwnz <marceschranz@tsn.at>
2022-11-23 09:54:24 +0100

    JDBC - Datenbankverbindung herstellen

--- jdbcdemo/src/main/java/JdbcDemo.java
+++ jdbcdemo/src/main/java/JdbcDemo.java
@@ -1,6 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcDemo {
 
     public static void main(String[] args) {
         System.out.println("JDBC DEMO!");
+
+        //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Marcel Schranz', 'marcel.schranz@myimst.at'), (NULL, 'Sarah Gosch', 'sarah.gosch@myimst.at');
+
+        selectAllDemo();
     }
+
+    public static void selectAllDemo(){
+        System.out.println("Select DEMO mit JDBC");
+        String sqlSelechtAllPersons = "SELECT * FROM `student`";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)){
+            System.out.println("Verbindung zur DB hergestellt!");
+        } catch(SQLException e){
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+
+
+
 }
