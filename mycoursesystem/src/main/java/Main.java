4a2b05c9d0f949a278dfd995acb3d87b4d4d2e2d
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2022-12-05 16:08:03 +0100

    JDBC_DAO - DB-Verbindung Singleton

--- mycoursesystem/src/main/java/Main.java
+++ mycoursesystem/src/main/java/Main.java
@@ -1,5 +1,22 @@
+import dataaccess.MysqlDatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST");
+
+        try {
+            Connection myConnection =
+                    MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/kurssystem", "root", "");
+            System.out.println("Verbindung zur DB erfolgreich aufgebaut");
+
+            //Durch die Singleton-Implementierung wird zur Laufzeit immer die gleiche Connection-Instanz zurückgegeben
+            //myConnection = MysqlDatabaseConnection.getConnection();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ mycoursesystem/src/main/java/dataaccess/MysqlDatabaseConnection.java
@@ -0,0 +1,27 @@
+package dataaccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlDatabaseConnection {
+    private static Connection con = null;
+
+    //Konstruktor ist privat, daher kann nicht mit new eine neue Instanz erzeugt werden
+    private MysqlDatabaseConnection(){
+
+    }
+
+    public static Connection getConnection(String url, String user, String pwd) throws ClassNotFoundException, SQLException {
+        if(con!=null){
+            return con;
+        } else {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pwd);
+            return con;
+        }
+    }
+
+
+
+}
