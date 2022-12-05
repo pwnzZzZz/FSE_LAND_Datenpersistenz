ddcbf1cf83f06b7b24910eaf6d97171c65bfff6f
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2022-12-05 16:32:01 +0100

    JDBC_DAO - Domänenklassen

---
+++ mycoursesystem/src/main/java/domain/Course.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.sql.Date;
+
+public class Course {
+    private String name;
+    private String description;
+    private int hours;
+    private Date beginDate;
+    private Date endDate;
+    private CourseType courseType;
+
+
+
+
+
+}
